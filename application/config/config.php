67913d370d1b3e25c3d323cf659ffc8b127a28db
Altug Orsmen <nickimnick@gmail.com>
2014-10-27 20:28:59 +0200

    MVS_Model
    Base model'ı oluşturdum. CRUD yapısını örenk dosyadan alıp bize
    uyarladım. Üsütne yine konuşur düzenleme ile devam ederiz

--- application/config/config.php
+++ application/config/config.php
@@ -1,5 +1,14 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+function __autoload($classname) {
+	if (strpos($classname, 'CI_') !== 0) {
+		$file = APPPATH . 'libraries/' . $classname . '.php';
+		if (file_exists($file) && is_file($file)) {
+			@include_once($file);
+		}
+	}
+}
+
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
@@ -106,7 +115,7 @@ $config['enable_hooks'] = FALSE;
 | http://codeigniter.com/user_guide/general/creating_libraries.html
 |
 */
-$config['subclass_prefix'] = 'MY_';
+$config['subclass_prefix'] = 'MVS_';
 
 
 /*
--- application/config/database.php
+++ application/config/database.php
@@ -54,7 +54,7 @@ $db['default']['password'] = 'qaptured1234';
 $db['default']['database'] = 'mvs_db';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
+$db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
@@ -62,7 +62,7 @@ $db['default']['char_set'] = 'utf8';
 $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
+$db['default']['stricton'] = TRUE;
 
 
 /* End of file database.php */
