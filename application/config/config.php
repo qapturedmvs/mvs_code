61cd6ba3c12ee31c166826b61c25326e5e32dbe6
Altug Orsmen <nickimnick@gmail.com>
2014-10-28 21:41:19 +0200

    düzenlemeler

--- application/config/config.php
+++ application/config/config.php
@@ -253,12 +253,12 @@ $config['encryption_key'] = 'gh9K*fHgZa2@hZc&hjasLuVfVBNa*%f';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'cisession';
+$config['sess_cookie_name']		= 'mvssession';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= TRUE;
 $config['sess_use_database']	= TRUE;
-$config['sess_table_name']		= 'mvs_sessions';
+$config['sess_table_name']		= 'mvs_adm_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
 $config['sess_time_to_update']	= 300;
--- application/controllers/admin/user.php
+++ application/controllers/admin/user.php
@@ -6,7 +6,7 @@ class User extends Backend_Controller {
     }
 
     public function login(){	
-        $this->data['subview'] = 'admin/pages/login_v';
+        $this->data['subview'] = 'admin/user/login';
         $this->load->view('admin/_main_body_layout', $this->data);
     }
 
