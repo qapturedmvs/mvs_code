806599575f13ff13eb7a6e5dbaa3bd404f5f6307
nickimnick <nickimnick@gmail.com>
2014-10-28 09:08:09 +0200

    Page

--- application/controllers/admin/user.php
+++ application/controllers/admin/user.php
@@ -6,8 +6,8 @@ class User extends Backend_Controller {
     }
 
     public function login(){	
-        $this->data['subview'] = 'admin/pages/user_login';
-        $this->load->view('admin/_layout_main', $this->data);
+        $this->data['subview'] = 'admin/pages/_view_login';
+        $this->load->view('admin/_main_body_layout', $this->data);
     }
 
 }
