6145ded254a4d6b1fce770a6b0527bde6dd957db
burakkarakaya <burakkarakaya1984@gmail.com>
2014-11-16 16:56:50 +0200

    angularjs seperator

--- application/views/movie/list.php
+++ application/views/movie/list.php
@@ -1,20 +1,16 @@
 <div ng-app='myApp' ng-controller='DemoController'>
-  <div infinite-scroll='reddit.nextPage()' infinite-scroll-disabled='reddit.busy' infinite-scroll-distance='1'>
+  <div infinite-scroll='reddit.nextPage()' infinite-scroll-disabled='reddit.busy' infinite-scroll-distance='0'>
     <div ng-repeat='item in reddit.items'>
-      <span class='score'>{{item.mvs_runtime}}</span>
-      <span class='title'>
-        <a ng-href='#' target='_blank'>{{item.mvs_title}}</a>
-      </span>
-      <small>by {{item.mvs_country}} -
-        <a ng-href='#' target='_blank'>{{item.cntry_id}} comments</a>
-      </small>
-      <div style='clear: both;'></div>
+      <div ng-if="item.type == 0 "> <span class='score'>{{item.mvs_runtime}}</span> <span class='title'> <a ng-href='#' target='_blank'>{{item.mvs_title}}</a> </span> <small>by {{item.mvs_country}} - <a ng-href='#' target='_blank'>{{item.cntry_id}} comments</a> </small>
+        <div style='clear: both;'></div>
+      </div>
+      <div ng-if="item.type == 1 " class="seperator">
+        <h2>{{item.paging}} . SAYFA</h2>
+      </div>
     </div>
     <div ng-show='reddit.busy'>Loading data...</div>
   </div>
 </div>
-
-
 <script type="text/javascript">
 
 /* http://binarymuse.github.io/ngInfiniteScroll/demo_async.html */
@@ -42,12 +38,13 @@ myApp.factory('Reddit', function($http) {
 	  if( d['result'] == 'OK' ){
 	  	for(var i = 0; i < d['data'].length; ++i){
 			var items = d['data'][ i ];
+				items['type'] = 0;			
 			this.items.push( items );
 		}
-	  	
 	  	this.after++;
-	  	this.busy = false;
-	  
+	  	this.busy = false;	
+		this.items.push( { 'type': 1, 'paging': this.after } );
+
 	  }
     }.bind(this));
   };
